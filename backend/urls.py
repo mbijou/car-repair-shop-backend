2b934b9fdb51e5d97c9d50067342cc81750fa965
mbijou <mimounbijjou@Mimouns-Air.local>
2020-10-23 01:56:41 +0200

    add static_url to urlpatterns

--- backend/urls.py
+++ backend/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('company.urls')),
     re_path(r'^$', TemplateView.as_view(template_name="angular.html")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
